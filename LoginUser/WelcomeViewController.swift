1fb86a6a3ada3b1bea730fd97ad2884cc0638d4f
OlDiKi <delorian101allgood@gmail.com>
2021-12-15 13:31:20 +0300

    setup mainboard ver 1.0.2

---
+++ LoginUser/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  LoginUser
+//
+//  Created by Дмитрий Олейнер on 15.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+   
